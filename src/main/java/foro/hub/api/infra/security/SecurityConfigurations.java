9d0071a5bf76141d0739c51c297fda869e74a21f
Aquatimez1991 <esalgadoc@outlook.com>
2025-08-06 18:06:30 -0500

    Remove CSRF disabling from security filter chain configuration

--- src/main/java/foro/hub/api/infra/security/SecurityConfigurations.java
+++ src/main/java/foro/hub/api/infra/security/SecurityConfigurations.java
@@ -37,7 +37,6 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
